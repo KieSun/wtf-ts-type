b47c72d3db80ffb5749e06f938d2e8131a19507e
KieSun <zx597813039@gmail.com>
2021-09-09 19:07:05 +0800

    feat: 18 - Length of Tuple

---
+++ src/medium/18 - Length of Tuple.ts
@@ -0,0 +1,45 @@
+/*
+  18 - Length of Tuple
+  -------
+  by sinoon (@sinoon) #easy #tuple
+  
+  ### Question
+  
+  For given a tuple, you need create a generic `Length`, pick the length of the tuple
+  
+  For example
+  
+  ```ts
+  type tesla = ['tesla', 'model 3', 'model X', 'model Y']
+  type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+  
+  type teslaLength = Length<tesla>  // expected 4
+  type spaceXLength = Length<spaceX> // expected 5
+  ```
+  
+  > View on GitHub: https://tsch.js.org/18
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+type Length<T extends any> = T extends readonly any[] ? T['length'] : never
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/18/answer
+  > View solutions: https://tsch.js.org/18/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
